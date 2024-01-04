fccda90c1deeb90dbd0e23169a24119dc59a8628
TradingForge <154653642+TradingForge-Co@users.noreply.github.com>
2024-01-04 10:41:48 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,16 +1,1 @@
-<script>
-(function() {
-    document.getElementById("downloadButton").addEventListener("click", function() {
-        // Replace the placeholder with the direct link to your Google Drive file
-        var downloadLink = "https://drive.google.com/u/0/uc?id=1UGDutNn4TgMG-6uBaKm-2xLol5Xlw1aB&export=download";
 
-        // Create a hidden iframe
-        var iframe = document.createElement("iframe");
-        iframe.style.display = "none";
-        document.body.appendChild(iframe);
-
-        // Set the iframe source to trigger the download
-        iframe.src = downloadLink;
-    });
-})();
-</script>
