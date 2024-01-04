3c17f385bd714d59fa7d98aa25fe0bfc21341131
TradingForge <154653642+TradingForge-Co@users.noreply.github.com>
2024-01-04 10:22:51 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,24 @@
-/*==================================================================================
-    Custom JS (Any custom js code you want to apply should be defined here).
-====================================================================================*/
-
+<script>
+(function() {
+    document.getElementById("downloadButton").addEventListener("click", function() {
+        // Replace the placeholder with the direct link to your Google Drive file
+        var downloadLink = "https://drive.google.com/file/d/1UGDutNn4TgMG-6uBaKm-2xLol5Xlw1aB/view?usp=sharing";
+        
+        // Create a temporary anchor element
+        var tempLink = document.createElement("a");
+        
+        // Set the download attribute and href of the anchor element
+        tempLink.setAttribute("download", "TradingForgeApp.zip");
+        tempLink.href = downloadLink;
+        
+        // Append the anchor element to the body
+        document.body.appendChild(tempLink);
+        
+        // Trigger a click event on the anchor element
+        tempLink.click();
+        
+        // Remove the anchor element from the body
+        document.body.removeChild(tempLink);
+    });
+})();
+</script>
